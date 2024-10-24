c338ee0831741db3e5196eaf98ea300cb0a5e8d5
Kir Piterov <k-piterov@yandex.ru>
2024-10-24 22:44:22 +0300

    see readme.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # qa_python
+Fix an error in example-test 'test_add_new_book_add_two_books'.
+Add conftest.py
+
+Add new tests:
+1. test_add_new_book_add_one_book_twice_gives_1
+2. 
--- tests.py
+++ tests.py
@@ -8,17 +8,21 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
+    def test_add_new_book_add_two_books(self,book):
         # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+        #collector = BooksCollector()
 
         # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        book.add_new_book('Гордость и предубеждение и зомби')
+        book.add_new_book('Что делать, если ваш кот хочет вас убить')
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(book.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_add_one_book_twice_gives_1(self,book):
+        book.add_new_book('Double')
+        book.add_new_book('Double')
+        assert len(book.get_books_genre()) == 1
