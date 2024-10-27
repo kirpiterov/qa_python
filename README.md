d84d9a759b21e94e96457b4b6a97f5242808a9df
Kir Piterov <k-piterov@yandex.ru>
2024-10-27 21:32:50 +0300

    see README

--- README.md
+++ README.md
@@ -12,3 +12,34 @@ Add new tests:
     При добавлении книги "без названия" (т.е. 0 символов) - должно быть добавлено 0 книг.
 1.4. 'test_add_new_book_added_book_have_no_genre_true'
     После добавления книги - ее жанр должен быть пустым.
+
+2.1. 'test_set_book_genre_if_genre_in_list_true'
+    Книге можно присвоить жанр, имеющийся в списке.
+2.2. 'test_set_book_genre_if_genre_not_in_list_no_add_book'
+    Книге нельзя присвоить жанр, которого нет в списке.
+
+3.1. 'test_get_book_genre_can_get_book_genre_by_her_name_true'
+    Можно получить жанр книги по ее имени.
+
+4.1. 'test_get_books_with_specific_genre_create_book_for_other_genre_and_get_true'
+    Можно получить список книг определенного жанра.
+
+5.1. 'test_get_books_genre_all_added_books_are_in_list_true'
+    Можно получить текущий словарь books_genre.
+
+6.1. 'def test_get_books_for_children_from_2_added_only_1_for_children_true'
+    Можно получить список детских книг(книги без возрастного рейтинга).
+
+7.1. 'test_add_book_in_favorites_added_book_is_in_favorite_true'
+    Книгу(даже без указания жанра) можно добавить в Избранное.
+
+7.2. 'test_add_book_in_favorites_add_one_book_twice_in_favorite_gives_1_true'
+    Книгу нельзя повторно добавить в Избранное.
+
+8.1. 'test_delete_book_from_favorites_add_2_delete_1_gives_1_true'
+    Книгу можно удалить из Избранного, если она там есть.
+
+9.1. 'test_get_list_of_favorites_books_add_2_books_and_check_list_true' 
+    Можно получить список Избранных книг.
+    
+    
--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
+import pytest
 from main import BooksCollector
+
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -24,13 +26,13 @@ class TestBooksCollector:
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
     #Тест 1.2. При добавлении одной и той же книги дважды - книга должна быть добавлена 1 раз.
-    def test_add_new_book_add_one_book_twice_gives_1(self,book):
+    def test_add_new_book_add_one_book_twice_gives_1_true(self,book):
         book.add_new_book('Double')
         book.add_new_book('Double')
         assert len(book.get_books_genre()) == 1
 
     # Тест 1.3. При добавлении книги "без названия" (т.е. 0 символов) - должно быть добавлено 0 книг.
-    def test_add_new_book_add_book_without_name_gives_0(self,book):
+    def test_add_new_book__add_book_without_name_gives_0__true(self,book):
         book.add_new_book('')
         assert len(book.get_books_genre()) == 0
 
@@ -38,3 +40,89 @@ class TestBooksCollector:
     def test_add_new_book_added_book_have_no_genre_true(self, book):
         book.add_new_book('Книга без жанра')
         assert book.get_books_genre().get('Книга без жанра') == ''
+
+    # Тест 2.1. Книге можно присвоить жанр, имеющийся в списке.
+    def test_set_book_genre_if_genre_in_list_true(self, book):
+        book_name = 'Книга с жанром, который есть в списке'
+        true_genre = 'Фантастика'
+        book.add_new_book(book_name)
+        book.set_book_genre(book_name,true_genre)
+        assert book.get_books_genre()['Книга с жанром, который есть в списке'] == true_genre
+
+    # Тест 2.2. Книге нельзя присвоить жанр, которого нет в списке.
+    def test_set_book_genre_if_genre_not_in_list_no_add_book_true(self, book):
+        book_name = 'Книга с жанром, которого нет в списке'
+        false_genre = 'Нежанр'
+        book.add_new_book(book_name)
+        book.set_book_genre(book_name,false_genre)
+        assert book.get_books_genre()['Книга с жанром, которого нет в списке'] == ''
+
+    # Тест 3.1. Можно получить жанр книги по ее имени.
+    def test_get_book_genre_can_get_book_genre_by_her_name_true(self, book):
+        book_name = 'Книга'
+        true_genre = 'Фантастика'
+        book.add_new_book(book_name)
+        book.set_book_genre(book_name,true_genre)
+        assert book.get_book_genre(book_name) == true_genre
+
+    # Тест 4.1. Можно получить список книг определенного жанра.
+    @pytest.mark.parametrize(
+        'book_name, book_genre',
+        [
+            ('Книга1', 'Фантастика'),
+            ('Книга2', 'Фантастика')
+        ]
+    )
+    def test_get_books_with_specific_genre_create_book_for_other_genre_and_get_true(self, book, book_name, book_genre):
+        book.add_new_book(book_name)
+        book.set_book_genre(book_name, book_genre)
+        assert book.get_books_with_specific_genre(book_genre) == [book_name]
+
+    # Тест 5.1. Можно получить текущий словарь books_genre
+    def test_get_books_genre_all_added_books_are_in_list_true(self, book):
+        book.add_new_book('Книга1')
+        book.set_book_genre('Книга1', 'Фантастика')
+        book.add_new_book('Книга2')
+        book.set_book_genre('Книга2', 'Фантастика')
+        assert book.get_books_genre() == {'Книга1': 'Фантастика', 'Книга2': 'Фантастика'}
+
+    # Тест 6.1. Можно получить список детских книг(книги без возрастного рейтинга)
+    def test_get_books_for_children_from_2_added_only_1_for_children_true(self, book):
+        book.add_new_book('Взрослая')
+        book.set_book_genre('Взрослая', 'Ужасы')
+        book.add_new_book('Детская')
+        book.set_book_genre('Детская', 'Мультфильмы')
+        assert book.get_books_for_children() == ['Детская']
+
+    # Тест 7.1. Книгу(даже без указания жанра) можно добавить в Избранное.
+    def test_add_book_in_favorites_added_book_is_in_favorite_true(self, book):
+        book_name = 'Избранная'
+        book.add_new_book(book_name)
+        book.add_book_in_favorites(book_name)
+        assert len(book.favorites) == 1
+
+    # Тест 7.2. Книгу нельзя повторно добавить в Избранное.
+    def test_add_book_in_favorites_add_one_book_twice_in_favorite_gives_1_true(self, book):
+        book_name = 'Избранная'
+        book.add_new_book(book_name)
+        book.set_book_genre(book_name, 'Ужасы')
+        book.add_book_in_favorites(book_name)
+        book.add_book_in_favorites(book_name)   #пытаемся добавить ту же книгу повторно
+        assert len(book.favorites) == 1
+
+    # Тест 8.1. Книгу можно удалить из Избранного, если она там есть
+    def test_delete_book_from_favorites_add_2_delete_1_gives_1_true(self, book):
+        book.add_new_book('Взрослая')
+        book.add_book_in_favorites('Взрослая')
+        book.add_new_book('Детская')
+        book.add_book_in_favorites('Детская')
+        book.delete_book_from_favorites('Взрослая') #две добавили, одну удалили
+        assert len(book.favorites) == 1
+
+    # Тест 9.1. Можно получить список Избранных книг.
+    def test_get_list_of_favorites_books_add_2_books_and_check_list_true(self, book):
+        book.add_new_book('Взрослая')
+        book.add_book_in_favorites('Взрослая')
+        book.add_new_book('Детская')
+        book.add_book_in_favorites('Детская')
+        assert book.get_list_of_favorites_books() == ['Взрослая','Детская']
